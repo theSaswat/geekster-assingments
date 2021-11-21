6bcc6cb96ffb42b7a2c21cbedecb7f903c1558f0
theSaswat <81437027+theSaswat@users.noreply.github.com>
2021-11-21 21:33:12 +0530

    Update 1st_Array_Generate.js

--- Problems/1st_Array_Generate.js
+++ Problems/1st_Array_Generate.js
@@ -1,1 +1,22 @@
+function range(start, end) 
+{
+  if (start > end) {
+ var arr = new Array(start - end + 1);
+	for (var i = 0; i < arr.length; i++, start--) {
+	resarrult[i] = start;
+}
+	return arr;
+} 
+  else{
+var array2 = new Array(end-start+1);
+         
+for (var j = 0; j < array2.length; j++, start++) 
+   {
+array2[j] = start;
+}
+      return array2;
+}
+}
 
+console.log(range(5,7));
+console.log(range(-6, 8));
