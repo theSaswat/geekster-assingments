db7ca288f0388a5bdeca77c84d37aca08d3d96ce
theSaswat <81437027+theSaswat@users.noreply.github.com>
2021-11-21 20:10:18 +0530

    Update boxPattern.js

--- Nov_21_Test/boxPattern.js
+++ Nov_21_Test/boxPattern.js
@@ -1,1 +1,14 @@
-
+function starpattern(){
+        var columns=8;
+	var rows=5;
+								
+	for(var i=1;i<=rows;i++)
+	{
+         for(var j=1;j<=columns;j++)
+         {
+          document.write("*");
+         }
+	  document.write("<br>");
+	 }
+   }
+starpattern()
