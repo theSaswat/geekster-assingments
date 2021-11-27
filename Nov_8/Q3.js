fb86f47c0d8cf81abb4fbe22bec7be4e1ab3e028
theSaswat <81437027+theSaswat@users.noreply.github.com>
2021-11-27 15:23:47 +0530

    Update Q3.js

--- Nov_8/Q3.js
+++ Nov_8/Q3.js
@@ -1,1 +1,19 @@
 
+function paintWall(n, h, w) {
+    
+    var areaOfWall = h * w;
+    return n/areaOfWall;
+}
+
+var n = 5;
+var h = 6;
+var w = 6;
+
+var totalWallPaint = paintWall(n, h, w);
+
+if (totalWallPaint > 0) {
+    console.log('total wall painted is --->' + totalWallPaint);
+}
+else {
+    console.log('You need to go shop for buy more paint');
+}
