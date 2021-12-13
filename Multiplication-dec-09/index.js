6da85a5807b89b35ec28dad504d8fcebfceca306
Saswat kumar Behera <81437027+theSaswat@users.noreply.github.com>
2021-12-13 14:23:11 +0530

    Update index.js

--- Multiplication-dec-09/index.js
+++ Multiplication-dec-09/index.js
@@ -1,1 +1,21 @@
+function mul(a) {
+    return function (b){
+        return function (c){
+            return a*b*c;
+         }
+     }
+ }
+let btn= document.getElementById('btn');
+btn.addEventListener('click', fn );
 
+     function fn(){
+        let a= document.getElementById('num1').value;
+        let b= document.getElementById('num2').value;
+        let c= document.getElementById('num3').value;
+
+        let d = mul(a)(b)(c);
+        console.log(d);
+
+        let y= document.getElementById('s1');
+        y.innerHTML= d;
+}
